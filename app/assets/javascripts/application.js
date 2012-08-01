e28f61976cbc12aef9f05b4148513ee9136989a2
Pedro Lopes <pedrolopes@staff-z2-918.wireless.ua.pt>
2012-08-01 17:03:39 +0100

    [LOVEGram] added initial files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,30 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function() {
+	$.getJSON("https://api.instagram.com/v1/tags/love/media/recent?client_id=54cb02eb1f784ea4bb750ea7c5abc85a&callback=?",
+	{
+    	format: "json"
+    	},
+    	function(data) {
+    		$.each(data.data, function(i, item) {    
+    			//alert(item.type);
+    			$('#images').append('<img src="' + item.images.standard_resolution.url + '" width="612" height="612" />');
+    		});
+	    }
+	 );
+	 
+	 $('#new').click(function(){
+	 	location.reload();
+	 });
+	 
+	 $('#me').click(function(){
+	 	window.location = "http://twitter.com/pedrolopes";
+	 });
+  });
+
+
+
+
+
+
