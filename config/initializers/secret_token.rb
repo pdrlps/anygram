39b1933886f0c0b531d56c50c4bc53f2c532d13e
Pedro Lopes <pedrolopes@staff-z2-918.wireless.ua.pt>
2012-08-01 15:36:49 +0100

    startup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovegram::Application.config.secret_token = 'da4dfd1bef28c95579ef6a6d156f61b9c7e7a170150bae483c955df30037159e76fe9be2416db4c6993b3fb98bec53d8ede6f9d06facf6b702a8645af0a7de11'
