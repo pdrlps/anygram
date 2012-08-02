5cbc350aa43f09924a92e8d339cb705d35f0d442
Pedro Lopes <hello@pedrolopes.net>
2012-08-02 16:27:59 +0100

    anygram: correct sample bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@tag = [:love, :infinity, :olympics, :gold, :silver, :portugal, :football, :soccer, :sports, :athlete, :medal]
-  	@photos = Instagram.tag_recent_media(@tag.choice)
+  	@photos = Instagram.tag_recent_media(@tag.sample)
   end
 end
