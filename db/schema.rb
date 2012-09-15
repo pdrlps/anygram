0d3cd30f68a348cdb46e4982e51d14adcd8373f8
Pedro Lopes <hello@pedrolopes.net>
2012-09-15 23:31:27 +0100

    removed captions

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,4 @@
 
 ActiveRecord::Schema.define(:version => 20120915172545) do
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.integer  "hits"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
